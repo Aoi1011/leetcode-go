14aa51a31c1848289ada498136bf6c35d6d9f22d
Aoi1011 <ht.so.ik0102@gmail.com>
2022-12-27 22:10:44 +0700

    Group Anagrams 2

--- arrays-hashing/group_anagrams.go
+++ arrays-hashing/group_anagrams.go
@@ -36,3 +36,22 @@ func GroupAnagrams(strs []string) [][]string {
 
 	return res
 }
+
+func GroupAnagrams2(strs []string) [][]string {
+	mp := make(map[[26]int][]string)
+
+	for _, s := range strs {
+		k := [26]int{}
+		for i := 0; i < len(s); i++ {
+			k[s[i]-'a'] += 1
+		}
+		mp[k] = append(mp[k], s)
+	}
+
+	res := [][]string{}
+	for _, v := range mp {
+		res = append(res, v)
+	}
+
+	return res
+}
--- arrays-hashing/group_anagrams_test.go
+++ arrays-hashing/group_anagrams_test.go
@@ -18,3 +18,17 @@ func TestGroupAnagrams(t *testing.T) {
 		t.Errorf("Err #1 %v, %v", res, output)
 	}
 }
+
+func TestGroupAnagrams2(t *testing.T) {
+	strs := []string{"eat", "tea", "tan", "ate", "nat", "bat"}
+	output := [][]string{}
+	output = append(output, []string{"bat"})
+	output = append(output, []string{"nat", "tan"})
+	output = append(output, []string{"ate", "eat", "tea"})
+
+	res := GroupAnagrams2(strs)
+
+	if !reflect.DeepEqual(res, output) {
+		t.Errorf("Err #1 %v, %v", res, output)
+	}
+}
