347351caaec3596b35fe35e7117401d2c7db2a36
Aoi1011 <ht.so.ik0102@gmail.com>
2022-12-29 22:40:49 +0700

    new solution

--- arrays-hashing/product_of_array_except_self.go
+++ arrays-hashing/product_of_array_except_self.go
@@ -23,6 +23,26 @@ func productExceptSelf(nums []int) []int {
 // solution 1
 func productExceptSelf1(nums []int) []int {
 	var res []int
+	total := 1
+	zeroCount := 0
+
+	for i := 0; i < len(nums); i++ {
+		if nums[i] == 0 {
+			zeroCount += 1
+			continue
+		}
+		total *= nums[i]
+	}
+
+	for i := 0; i < len(nums); i++ {
+		if zeroCount == 0 {
+			res = append(res, total/nums[i])
+		} else if zeroCount == 1 && nums[i] == 0 {
+			res = append(res, total)
+		} else {
+			res = append(res, 0)
+		}
+	}
 
 	return res
 }
