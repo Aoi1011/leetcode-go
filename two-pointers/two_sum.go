bfdd7bbcdb5e7ab43c3c603acc7f4ba412a295a6
Aoi1011 <ht.so.ik0102@gmail.com>
2023-01-06 09:19:08 +0700

    Implement another solution

--- two-pointers/two_sum.go
+++ two-pointers/two_sum.go
@@ -21,3 +21,24 @@ func twoSum(numbers []int, target int) []int {
 
 	return res
 }
+
+func TwoSum1(numbers []int, target int) []int {
+	var res []int
+	left := 0
+	right := len(numbers) - 1
+
+	for left < right {
+		currentSum := numbers[left] + numbers[right]
+
+		if currentSum == target {
+			res = append(res, left+1, right+1)
+			break
+		} else if currentSum < target {
+			left++
+		} else {
+			right--
+		}
+	}
+
+	return res
+}
